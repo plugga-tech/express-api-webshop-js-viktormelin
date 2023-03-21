246520da728953ba9f72f79a2563a53ce102dc8b
dough <viktor.melin@me.com>
2023-03-21 12:04:57 +0100

    (backend): implemented better error handler

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import connectDatabase from './config/database';
 import userRouter from './routes/user.routes';
+import errorHandler from './middlewares/error.middleware';
 
 const PORT = 3000;
 
@@ -12,6 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/users', userRouter);
+
+app.use(errorHandler);
 
 app.listen(PORT, async () => {
   console.log(`Server started on port: ${PORT}`);
