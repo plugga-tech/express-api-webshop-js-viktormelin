fd6ee385e97775423760403234b419331c9cfdd9
dough <viktor.melin@me.com>
2023-03-22 09:23:38 +0100

    (backend): fix type for objectId

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -1,8 +1,9 @@
 import * as jose from 'jose';
+import { Types } from 'mongoose';
 
 const secret = new TextEncoder().encode(process.env.PRIVATE_KEY);
 
-export const signJWT = async (id: string) => {
+export const signJWT = async (id: Types.ObjectId) => {
   return await new jose.SignJWT({ id }).setProtectedHeader({ alg: 'HS256' }).setExpirationTime('6h').sign(secret);
 };
 
