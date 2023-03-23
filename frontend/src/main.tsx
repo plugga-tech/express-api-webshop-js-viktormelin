59fa5915dfc0a2a2df0ea406e3545787bbe9ec8b
dough <viktor.melin@me.com>
2023-03-23 20:42:50 +0100

    (frontend): update routes with new pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,16 +5,42 @@ import './index.scss';
 import '@fontsource/rubik';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Root from './routes/Root';
+import AuthContext from './utils/AuthContext';
+import Login from './routes/Login';
+import Admin from './routes/Admin';
+import useUser from './hooks/useUser';
+import Register from './routes/Register';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
   },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/admin',
+    element: <Admin />,
+  },
 ]);
+
+const App = () => {
+  const { user } = useUser();
+  return (
+    <AuthContext.Provider value={{ user }}>
+      <RouterProvider router={router} />
+    </AuthContext.Provider>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
