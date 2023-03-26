ef35220c81c58b0a9d6e1de63c352762158f65f3
dough <viktor.melin@me.com>
2023-03-26 13:01:20 +0200

    (frontend): layout changes to products

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { Product } from '../types/typings';
 import useCart from '../hooks/useCart';
+import { internationlizeCurrency } from '../utils/helpers';
+import useProducts from '../hooks/useProducts';
 
 const ProductCard = ({ product }: { product: Product }) => {
   const { addToCart } = useCart();
@@ -28,13 +30,15 @@ const ProductCard = ({ product }: { product: Product }) => {
     <div className='category__products_product' key={product._id}>
       <img src='http://via.placeholder.com/170x120' alt='' />
       <p>{product.name}</p>
-      <p>{product._id}</p>
+      <p>{internationlizeCurrency(product.price)}</p>
       <div>
         <button onClick={decreaseCount}>-</button>
         <input type='number' value={count} onChange={(e) => setCount(Number(e.currentTarget.value))} />
         <button onClick={increaseCount}>+</button>
       </div>
-      <button onClick={handleAddToCart}>Lägg till i kundvagn</button>
+      <button onClick={handleAddToCart} disabled={count > product.lager}>
+        Lägg till i kundvagn
+      </button>
     </div>
   );
 };
